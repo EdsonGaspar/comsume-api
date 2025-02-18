0bf3de1be6eca5ef4516c8a1637f917665c386c4
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2025-02-18 19:09:48 +0100

    Feat: Componente principal mais com probrlema na renderização do map

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,8 @@
-export function Header() {
+export function Cabecalho() {
   return (
     <header className="text-white">
-      <h1>
-        <span className="text-[#938878]">Categoria</span>: Ação
+      <h1 className="text-4xl p-5">
+        <span className="text-[#938878] ">Categoria</span>: Ação
       </h1>
     </header>
   );
