aae89c78230f4a40a150f22903589f25b188dc4b
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2025-02-18 18:07:17 +0100

    Feat: Adicionando componente Header.

---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header className="text-white">
+      <h1>
+        <span className="text-[#938878]">Categoria</span>: Ação
+      </h1>
+    </header>
+  );
+}
